41a407bc1adfbd39dd7f61e6cc9398f364893007
jhy439 <61407637+jhy439@users.noreply.github.com>
2021-09-29 13:06:05 +0900

    # of ghosts moving randomly

---
+++ GDP-VRMobile/Assets/Scripts/MoveRandomly.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveRandomly : MonoBehaviour
+{
+    NavMeshAgent navMeshAgent;
+    public float timeForNewPath;
+    bool inCoRoutine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!inCoRoutine) StartCoroutine(coRoutine());
+    }
+
+    Vector3 getNewRandomPosition(){
+        float x = Random.Range(-20, 20);
+        float z = Random.Range(-20, 20);
+        
+        Vector3 pos = new Vector3(x, 0, z);
+        return pos;
+    }
+
+    IEnumerator coRoutine(){
+        inCoRoutine = true;
+        yield return new WaitForSeconds(timeForNewPath);
+        GetNewPath();
+        inCoRoutine = false;
+    }
+
+    void GetNewPath(){
+        navMeshAgent.SetDestination(getNewRandomPosition());
+    }
+}
