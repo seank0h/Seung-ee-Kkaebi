7628745f4170d6d9fc58f4ff5bddd6cb98f73486
KangSeokjun <46084317+KangSeokjun@users.noreply.github.com>
2021-10-27 19:57:24 +0900

    double raytracing

--- GDP-VRMobile/Assets/Scripts/RayCast_cam.cs
+++ GDP-VRMobile/Assets/Scripts/RayCast_cam.cs
@@ -2,10 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RayCasting : MonoBehaviour
+public class RayCast_cam : MonoBehaviour
 {
     RaycastHit hit;
-    Vector3 height = new Vector3(0,1.375f,0);
+    Vector3 height = new Vector3(0, 0, 0);
     bool reset = false;
     Behaviour halo;
 
@@ -13,28 +13,28 @@ public class RayCasting : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         Dance();
-        
+
     }
 
     private void Dance()
     {
-        if (Physics.Raycast(gameObject.transform.position + height, gameObject.transform.forward, out hit, 1000))
+        if (Physics.Raycast(gameObject.transform.position, gameObject.transform.forward, out hit, 1000))
         {
-            Debug.DrawRay(transform.position + height, transform.forward * 1000, Color.red);
+            Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward * 1000, Color.red);
             if (hit.collider.tag == "Interactive")
             {
                 Debug.Log(hit.collider.name);
                 Debug.Log(hit.distance);
-                if (hit.distance <= 3)
+                if (hit.distance <= 100000)
                 {
-                    Debug.DrawRay(transform.position + height, transform.forward * 1000, Color.yellow);
+                    Debug.DrawRay(transform.position, transform.forward * 1000, Color.yellow);
                     halo = (Behaviour)hit.transform.gameObject.GetComponent("Halo");
                     halo.enabled = true;
                     reset = true;
