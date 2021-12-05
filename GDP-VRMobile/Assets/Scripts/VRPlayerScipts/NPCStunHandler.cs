9ae23386c8ca84db6269d58a6c63af03a2d4b9ca
seank0h <31454156+seank0h@users.noreply.github.com>
2021-12-06 01:19:53 +0900

    StatusMat

--- GDP-VRMobile/Assets/Scripts/VRPlayerScipts/NPCStunHandler.cs
+++ GDP-VRMobile/Assets/Scripts/VRPlayerScipts/NPCStunHandler.cs
@@ -8,63 +8,24 @@ public class NPCStunHandler : MonoBehaviour
     //public List<GameObject> NPCList;
     // Start is called before the first frame update
     public SkinnedMeshRenderer npcRenderer;
+    public GameObject gaugeBall;
+    private Renderer gaugeBallMaterial;
     void Start()
     {
-
-        /*
-        for (int i = 0; i < NPCList.Count; i++)
-        {
-            NPCList[i].gameObject.GetComponent<Renderer>().material.color = Color.black;
-        }
-        */
-
+        gaugeBallMaterial = gaugeBall.GetComponent<Renderer>();
+        
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
-        //Debug.Log("Stun Check: "+mobile2vr.mobileToVRCl.NPCStunState());
-        /*
-        if (mobile2vr.mobileToVRCl.NPCStunState() == 0)
-        {
-            NPCList[0].gameObject.GetComponent<Renderer>().material.color = Color.red;
-            NPCList[0].gameObject.GetComponent<NavMeshAgent>().speed = 0;
-            NPCList[0].gameObject.GetComponent<CapsuleCollider>().enabled = false;
-            mobile2vr.mobileToVRCl.npcStunState = -1;
-        }
-        else if (mobile2vr.mobileToVRCl.NPCStunState() == 1)
-        {
-            NPCList[1].gameObject.GetComponent<Renderer>().material.color = Color.red;
-            NPCList[1].gameObject.GetComponent<NavMeshAgent>().speed = 0;
-            NPCList[1].gameObject.GetComponent<CapsuleCollider>().enabled = false;
-            mobile2vr.mobileToVRCl.npcStunState = -1;
-        }
-        else if (mobile2vr.mobileToVRCl.NPCStunState() == 2)
-        {
-            NPCList[2].gameObject.GetComponent<Renderer>().material.color = Color.red;
-            NPCList[2].gameObject.GetComponent<NavMeshAgent>().speed = 0;
-            NPCList[2].gameObject.GetComponent<CapsuleCollider>().enabled = false;
-            mobile2vr.mobileToVRCl.npcStunState = -1;
-        }
-        else if (mobile2vr.mobileToVRCl.NPCStunState() == 3)
-        {
-            NPCList[3].gameObject.GetComponent<Renderer>().material.color = Color.red;
-            NPCList[3].gameObject.GetComponent<NavMeshAgent>().speed = 0;
-            NPCList[3].gameObject.GetComponent<CapsuleCollider>().enabled = false;
-            mobile2vr.mobileToVRCl.npcStunState = -1;
-        }
-        else if (mobile2vr.mobileToVRCl.NPCStunState() == 4)
-        {
-            NPCList[4].gameObject.GetComponent<Renderer>().material.color = Color.red;
-            NPCList[4].gameObject.GetComponent<NavMeshAgent>().speed = 0;
-            NPCList[4].gameObject.GetComponent<CapsuleCollider>().enabled = false;
-            mobile2vr.mobileToVRCl.npcStunState = -1;
-        }
-        */
+       
     }
     public void StunState()
     {
-       npcRenderer.material.color = Color.red;
+        float fillAmount = gaugeBallMaterial.material.GetFloat("_CuttOff") - 0.25f;
+        gaugeBallMaterial.material.SetFloat("_CutOff", fillAmount);
+        npcRenderer.material.color = Color.red;
        this.gameObject.GetComponent<NavMeshAgent>().speed = 0;
        this.gameObject.GetComponent<CapsuleCollider>().enabled = false;
     }
