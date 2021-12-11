7c4e26d6dcd885070a9316e710b39ca27a603502
seank0h <31454156+seank0h@users.noreply.github.com>
2021-12-11 20:26:32 +0900

    Added Mobile Minimap and Recreated Circular Level

---
+++ GDP-VRMobile/Assets/Minimap.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
+    }
+}
