88fe0cd4fb8f5ef80d2f8dab3709bf0403b3594c
seank0h <31454156+seank0h@users.noreply.github.com>
2021-12-15 19:46:06 +0900

    Remove some unused scenes/code and fixed rotation

--- GDP-VRMobile/Assets/Server/vrClient.cs
+++ GDP-VRMobile/Assets/Server/vrClient.cs
@@ -73,7 +73,7 @@ public class vrClient : MonoBehaviour{
                 continue;
 
             player.GetComponent<Transform>().position = new Vector3(float.Parse(msg[i][1]), float.Parse(msg[i][2])+0.5f, float.Parse(msg[i][3]));
-            player.GetComponent<Transform>().eulerAngles = new Vector3(float.Parse(msg[i][4]), float.Parse(msg[i][5]), float.Parse(msg[i][6]));
+            player.GetComponent<Transform>().eulerAngles = new Vector3(float.Parse(msg[i][4]), float.Parse(msg[i][5])+180, float.Parse(msg[i][6]));
 
             
             setPlayerMat(int.Parse(msg[i][7]));
