cba3d2dcf7a148d776a3849d62473e214e997fcc
seank0h <31454156+seank0h@users.noreply.github.com>
2021-12-16 15:46:19 +0900

    MainMenuImplementation

--- GDP-VRMobile/Assets/Scripts/MainMenuController.cs
+++ GDP-VRMobile/Assets/Scripts/MainMenuController.cs
@@ -8,16 +8,39 @@ public class MainMenuController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
-    public void LoadGame()
+    public void LoadGameMobile()
     {
         SceneManager.LoadScene("Greybox_mobile");
+    }
+    public void LoadGameVR()
+    {
+        SceneManager.LoadScene("GreyboxV3");
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Collision Triggering");
+        if(other.name=="PlayTag")
+        {
+            Debug.Log("Touching PlayTag");
+            LoadGameVR();
+        }
+        if(other.name=="ExitTag")
+        {
+            Debug.Log("Touching ExitTag");
+            QuitGame();
+        }
     }
 }
